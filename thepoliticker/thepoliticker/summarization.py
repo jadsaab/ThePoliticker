2842fe8c4b16b5d941321cb1b33063ff08c57c82
jadsaab <jadsaabmail@gmail.com>
2019-10-08 14:12:05 -0400

    Refactored TextRank and BERT summarization code. Added elbox analysis for selecting the number of clusters in BERT summarization.

--- thepoliticker/thepoliticker/summarization.py
+++ thepoliticker/thepoliticker/summarization.py
@@ -9,8 +9,11 @@ from nltk import tokenize
 from lxml import html
 import requests
 import re
+from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
 
-num_sentences = 4
+
+max_sentences = 4
 
 # Load embedder
 embedder = SentenceTransformer('bert-base-nli-mean-tokens')
@@ -104,64 +107,94 @@ def query_dataframe(df, speakername, topic):
     topic
     """
 
-    return df['speechtext'][df['subtopic'] == topic][df['speakername'] == speakername]
+    # Two different queries to avoid issues with boolean indexing
+    speeches = df['speechtext'][df['subtopic'] == topic][df['speakername'] == speakername]
+    dates = df['speechdate'][df['subtopic'] == topic][df['speakername'] == speakername]
 
+    return speeches, dates
 
-def summarize_with_textrank(df, speakername, topic):
+def run_textrank(text_to_summarize, num_sentences = max_sentences):
     """
     Function that produces a summary using the TextRank algorithm
     """
 
-    speeches = query_dataframe(df, speakername, topic)
-
-    # Store speeches as a concatenated string (to be passed to TextRank)
-    # Store speeches as list (to be returned by this function)
-    original_passages = []
-    text_to_summarize = ''
-    count = 0
-    for index, value in speeches.items():
-        original_passages.append(value)
-        text_to_summarize = text_to_summarize + ' ' + value
-        count += 1
-
-    raw_summary = summarize(text_to_summarize, ratio=0.2)
+    raw_summary = summarize(text_to_summarize, ratio=0.25)
     tokenized_sentences = tokenize.sent_tokenize(raw_summary)
+    print(type(tokenized_sentences))
 
     if len(tokenized_sentences) > num_sentences:
-        summary = ' '.join(tokenized_sentences[0:num_sentences])
+
+        #summary = ' '.join(tokenized_sentences[0:num_sentences])
+        summary = tokenized_sentences[0:num_sentences]
     else:
-        summary = ' '.join(tokenized_sentences)
+        #summary = ' '.join(tokenized_sentences)
+        summary = tokenized_sentences
 
-    print(summary)
-    return original_passages, summary
+    return summary, num_sentences;
 
 
-def summarize_with_BERT(df, speakername, topic):
+def run_BERT(text_to_summarize, max_sentences = max_sentences):
     """
     Function that produces a summary by first embedding sentences using BERT,
     clustering those embeddings, and selecting the sentence from each cluster
     that's closest to the centroid
     """
-
-    speeches = query_dataframe(df, speakername, topic)
-
-    # Store speeches as a concatenated string (to be split, by sentence, for
-    # embedding). Also, store speeches as list (to be returned by this function)
-    text_to_summarize = ''
-    original_passages =[]
-    for index, value in speeches.items():
-        original_passages.append(value)
-        text_to_summarize = text_to_summarize + ' ' + value
-
     # Segment concatenated string into sentences
     tokenized_sentences = tokenize.sent_tokenize(text_to_summarize)
-    speech_embeddings = embedder.encode(tokenized_sentences)
+    print(len(tokenized_sentences))
+    speech_embeddings = np.array(embedder.encode(tokenized_sentences))
+    print(speech_embeddings.shape)
 
     # Perform K-Means clustering. Summary will consist of sentence in each
     # cluster that is closest to the centroid
-    num_clusters = num_sentences
-    km = KMeans(num_clusters)
+    #num_clusters = max_sentences
+    #km = KMeans(num_clusters)
+    #preds = km.fit_predict(speech_embeddings)
+
+    distortions = []
+    print(max_sentences)
+    K = range(1, max_sentences+1)
+    for k in K:
+        km = KMeans(n_clusters=k).fit(speech_embeddings)
+        km.fit(speech_embeddings)
+        distortions.append(sum(np.min(cdist(speech_embeddings, km.cluster_centers_, 'euclidean'), axis=1)) / speech_embeddings.shape[0])
+
+    # Compute first and second numerical derivatives
+    distortion_prime = [a - b for a, b in zip(distortions[1:], distortions[0:-1])]
+    distortion_prime.insert(0, 0)
+
+    distortion_double_prime = [a - b for a, b in zip(distortion_prime[1:], distortion_prime[0:-1])]
+    distortion_double_prime.insert(0, 0)
+
+    plt.subplot(3,1,1)
+    plt.plot(K, distortion_prime, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('distortion_prime')
+    plt.title('distortion_prime')
+    plt.show()
+
+    # Plot the elbow
+    plt.subplot(3,1,2)
+    plt.plot(K, distortions, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Distortion')
+    plt.title('The Elbow Method showing the optimal k')
+    plt.show()
+
+    plt.subplot(3,1,3)
+    plt.plot(K, distortion_double_prime, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('distortion_double_prime')
+    plt.title('distortion_double_prime')
+    plt.show()
+
+    # Identify elbow/ inflection using second derivative
+    num_clusters = np.argmax(np.array(distortion_double_prime), axis=0)
+
+    # Cluster using empirically determined number of clusters
+    km = KMeans(int(num_clusters))
     preds = km.fit_predict(speech_embeddings)
+
     summary = ''
     for this_label in range(num_clusters):
         mask = preds == this_label
@@ -171,4 +204,40 @@ def summarize_with_BERT(df, speakername, topic):
         if best_sentence != '':
             summary += best_sentence + ' '
 
-    return original_passages, summary
+    return summary, num_clusters
+
+
+def summarize_speeches(df, speakername, topic, method='TextRank'):
+    """
+    Function that collects an MP's speeches on a given topic then passes the
+    text to a summarization algorithm
+    """
+
+    speeches, dates = query_dataframe(df, speakername, topic)
+    speeches = speeches.iloc[::-1]
+    dates = dates.iloc[::-1]
+
+    # Store speeches as a concatenated string (to be passed to TextRank)
+    # Store speeches as list (to be returned by this function)
+    original_passages = []
+    original_dates = []
+    text_to_summarize = ''
+    count = 0
+    for index, value in speeches.items():
+        original_passages.append(value)
+        text_to_summarize = text_to_summarize + ' ' + value
+        count += 1
+
+    for index, value in dates.items():
+        original_dates.append(value)
+
+    summary = ''
+    if method == 'TextRank':
+        summary = run_textrank(text_to_summarize)
+
+    elif method == "BERT":
+        summary = run_BERT(text_to_summarize)
+    else:
+        print('Unknown method for summarization, \'%s\'' % method)
+
+    return original_passages, original_dates, summary
